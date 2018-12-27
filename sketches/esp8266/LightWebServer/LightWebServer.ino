650b6cdba486a3c126ecf8cfa5927a45bb5454ec
kalemena <kalemena@free.fr>
2018-12-27 13:24:57 +0100

    Add Candle effect

--- sketches/esp8266/LightWebServer/LightWebServer.ino
+++ sketches/esp8266/LightWebServer/LightWebServer.ino
@@ -1,3 +1,4 @@
+#define FASTLED_INTERRUPT_RETRY_COUNT 0
 #define FASTLED_ALLOW_INTERRUPTS 0
 #include "FastLED.h"
 //#include <Adafruit_NeoPixel.h>
@@ -115,7 +116,7 @@ void loop(void) {
     
     inProgress = true;
     switch(selectedEffect) {
-      case 0: eRGBLoop(); break;
+      case 0: eCandle(); break;
       case 1: eFadeInOut(0xff, 0x00, 0x00); eFadeInOut(0xff, 0xff, 0xff); eFadeInOut(0x00, 0x00, 0xff); break;
       case 2: eStrobe(0xff, 0xff, 0xff, 10, 50, 1000); break;
       case 3: eHalloweenEyes(0xff, 0x00, 0x00, 1, 4, true, random(5,50), random(50,150), random(1000, 10000));
@@ -143,6 +144,7 @@ void loop(void) {
       //case 23: eRing(500); break;
       //case 24: eDrop(500); break;
       case 22: eFire(55,120,15); break;
+      case 23: eRGBLoop(); break;
     }
     inProgress = false;
   }
@@ -158,6 +160,9 @@ boolean tick() {
 }
 
 // ==== Tools
+
+#define MINVAL(A,B)             (((A) < (B)) ? (A) : (B))
+#define MAXVAL(A,B)             (((A) > (B)) ? (A) : (B))
 
 // ==== Web Controllers
 
@@ -192,6 +197,7 @@ void handle_Root() {
   result += "</html>";
   
   server.send(200, "text/html", result);
+  // server.send ( 200, "text/html","<SCRIPT language='JavaScript'>window.location='/';</SCRIPT>");
   digitalWrite(STATUS_LED, 0);
 }
 
@@ -233,6 +239,8 @@ void handle_SwitchEffect() {
     selectedEffect=0;
   }
   EEPROM.put(0, selectedEffect);
+  //EEPROM.write(0, selectedEffect);
+  //EEPROM.commit();
   effectChanged = true;
 
   String json = "{\n";
@@ -923,6 +931,114 @@ void eLightning(uint8_t wait) {
   }
 }
 */
+
+// The LED can be in only one of these states at any given time
+#define BRIGHT                  0
+#define UP                      1
+#define DOWN                    2
+#define DIM                     3
+#define BRIGHT_HOLD             4
+#define DIM_HOLD                5
+
+#define INDEX_BOTTOM_PERCENT    10 // Percent chance the LED will suddenly fall to minimum brightness
+#define INDEX_BOTTOM            128// Absolute minimum red value (green value is a function of red's value)
+#define INDEX_MIN               192// Minimum red value during "normal" flickering (not a dramatic change)
+#define INDEX_MAX               255// Maximum red value
+#define DOWN_MIN_MSECS          20 // Decreasing brightness will take place over a number of milliseconds in this range
+#define DOWN_MAX_MSECS          250
+#define UP_MIN_MSECS            20 // Increasing brightness will take place over a number of milliseconds in this range
+#define UP_MAX_MSECS            250
+#define BRIGHT_HOLD_PERCENT     20 // Percent chance the color will hold unchanged after brightening
+#define BRIGHT_HOLD_MIN_MSECS   0  // When holding after brightening, hold for a number of milliseconds in this range
+#define BRIGHT_HOLD_MAX_MSECS   100
+#define DIM_HOLD_PERCENT        5  // Percent chance the color will hold unchanged after dimming
+#define DIM_HOLD_MIN_MSECS      0  // When holding after dimming, hold for a number of milliseconds in this range
+#define DIM_HOLD_MAX_MSECS      50
+ 
+#define CANDLEPIXELS 60
+
+byte state, index_start, index_end;
+unsigned long flicker_msecs;
+unsigned long flicker_start;
+
+void set_color(byte index) {
+  int i; 
+  index = MAXVAL(MINVAL(index, INDEX_MAX), INDEX_BOTTOM);
+  if (index >= INDEX_MIN) {
+    for(i=0;i<CANDLEPIXELS;i++) {
+      setPixel(i, index, (index * 3) / 8, 0);      
+    }
+  } else if (index < INDEX_MIN) {
+    for(i=0;i<CANDLEPIXELS;i++) {
+      setPixel(i, index, (index * 3.25) / 8, 0);
+    }
+  } 
+  return;
+}
+
+void eCandle() {
+  while(true) {
+    unsigned long current_time; 
+    current_time = millis();
+  
+    switch(state) {
+      case BRIGHT:   
+        flicker_msecs = random(DOWN_MAX_MSECS - DOWN_MIN_MSECS) + DOWN_MIN_MSECS;
+        flicker_start = current_time;
+        index_start = index_end;
+        if ((index_start > INDEX_BOTTOM) && (random(100) < INDEX_BOTTOM_PERCENT))
+          index_end = random(index_start - INDEX_BOTTOM) + INDEX_BOTTOM;
+        else
+          index_end = random(index_start - INDEX_MIN) + INDEX_MIN;
+        state = DOWN;
+        break;  
+      
+      case DIM:
+        flicker_msecs = random(UP_MAX_MSECS - UP_MIN_MSECS) + UP_MIN_MSECS;
+        flicker_start = current_time;
+        index_start = index_end;
+        index_end = random(INDEX_MAX - index_start) + INDEX_MIN;
+        state = UP;
+        break;
+    
+      case BRIGHT_HOLD:  
+      case DIM_HOLD:
+         if (current_time >= (flicker_start + flicker_msecs))
+          state = (state == BRIGHT_HOLD) ? BRIGHT : DIM; 
+         break;
+   
+      case UP:
+      case DOWN:
+        if (current_time < (flicker_start + flicker_msecs)) {
+          set_color(index_start + ((index_end - index_start) * (((current_time - flicker_start) * 1.0) / flicker_msecs)));
+          break;
+        }
+       
+        set_color(index_end);
+        if (state == DOWN) {
+          if (random(100) < DIM_HOLD_PERCENT) {
+            flicker_start = current_time;
+            flicker_msecs = random(DIM_HOLD_MAX_MSECS - DIM_HOLD_MIN_MSECS) + DIM_HOLD_MIN_MSECS;
+            state = DIM_HOLD;
+          } else {
+            state = DIM;
+          }
+        } else {
+          if (random(100) < BRIGHT_HOLD_PERCENT) {
+            flicker_start = current_time;
+            flicker_msecs = random(BRIGHT_HOLD_MAX_MSECS - BRIGHT_HOLD_MIN_MSECS) + BRIGHT_HOLD_MIN_MSECS;
+            state = BRIGHT_HOLD;
+          } else {
+            state = BRIGHT;
+          }
+        }
+        break; 
+    }
+    showStrip();
+    if(tick()) return;
+  }
+}
+
 // ***************************************
 // ** FastLed/NeoPixel Common Functions **
 // ***************************************
