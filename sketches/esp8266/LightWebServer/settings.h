e909ee2bf6da7126a07c5044a5b89210ec5e2938
kalemena <kalemena@free.fr>
2019-03-03 21:43:29 +0100

    testing 150 leds

--- sketches/esp8266/LightWebServer/settings.h
+++ sketches/esp8266/LightWebServer/settings.h
@@ -1,7 +1,7 @@
 #include "settings-wifi.h"
 
 #define LED_PIN         D2
-#define NUM_LEDS        60
+#define NUM_LEDS        150
 #define MILLI_AMPS      2000 // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
 #define STATUS_LED      13
 
